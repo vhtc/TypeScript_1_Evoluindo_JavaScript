8b29c5970e9100ffeb77168c75c82dcb6b4c4490
vhtc <victor.tuy@hotmail.com>
2024-11-19 22:51:28 -0300

    curso finalizado

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,27 +1,40 @@
 export class Negociacao {
-    private _data: Date;
-    private _quantidade: number;
-    private _valor: number;
+    // private _data: Date;
+    // private _quantidade: number;
+    // private _valor: number;
 
-    constructor(data: Date, quantidade: number, valor: number) {
-        this._data = data
-        this._quantidade = quantidade
-        this._valor = valor
-    }
+    // constructor(data: Date, quantidade: number, valor: number) {
+    //     this._data = data
+    //     this._quantidade = quantidade
+    //     this._valor = valor
+    // }
 
-    get data(): Date {
-        return this._data
-    }
+    // constructor(private _data: Date, private _quantidade: number, private _valor: number){} //faz a mesma coisa que o codigo comentado acima, porem mais limpo
 
-    get quantidade(): number {
-        return this._quantidade
-    }
+    // constructor(public readonly data: Date, public readonly quantidade: number, public readonly valor: number) { } //faz a mesma coisa que o codigo comentado acima, porem mais limpo
 
-    get valor(): number {
-        return this._valor
-    }
+    constructor(private _data: Date, public readonly quantidade: number, public readonly valor: number) { } //faz a mesma coisa que o codigo comentado acima, porem mais limpo
+
+    // get data(): Date {
+    //     return this._data
+    // }
+
+    // get quantidade(): number {
+    //     return this._quantidade
+    // }
+
+    // get valor(): number {
+    //     return this._valor
+    // }
 
     get volume(): number {
-        return this._quantidade * this._valor
+        // return this._quantidade * this._valor
+        return this.quantidade * this.valor
+
+    }
+
+    get data(): Date{
+        const data = new Date(this._data.getTime())
+        return data
     }
 }
--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,18 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-    private _negociacoes: Array<Negociacao> = []
+    // private _negociacoes: Array<Negociacao> = []
+    private _negociacoes: Negociacao[] = [] //mesmo codigo que o de cima, mais simples de fazer
 
     adiciona(negociacao: Negociacao) {
         this._negociacoes.push(negociacao)
     }
 
-    lista(): ReadonlyArray<Negociacao> { //lista somente leitura (ReadonlyArray)
+    // lista(): ReadonlyArray<Negociacao> { //lista somente leitura (ReadonlyArray)
+    //     return this._negociacoes
+    // }
+
+    lista(): readonly Negociacao[]{ //mesmo codigo de cima, mais facil
         return this._negociacoes
     }
 }
